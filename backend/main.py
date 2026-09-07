6851634600c6bc69e328547f6952cabf88ee7846
PARTH PATHAK <71459513+parth1234560@users.noreply.github.com>
2026-09-07 19:15:03 +0530

    Add new CORS origin for production(render) frontend

--- backend/main.py
+++ backend/main.py
@@ -49,6 +49,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://sih26092-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
